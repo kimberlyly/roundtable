e2dbd1fc6a10c55b3de85840fc8fa9daca86c205
Kimberly Ly <kimberly.ly95@gmail.com>
2016-02-25 16:48:27 -0800

    Initial commits
    Basic structure

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var handlebars = require('express3-handlebars');
+var io = require('socket.io')(http);
+var index = require('./routes/index');
+//var chat = require('./routes/chat');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', index.view);
+
+//app.get('/chat', chat.view);
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+});
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+});
